416dd1b4764fe7e384681977673583175fbf0cb0
Padilla-Avilés <42844765+Axioma42@users.noreply.github.com>
2019-06-24 21:33:59 -0500

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,36 +2,59 @@ import os
 import csv
 from statistics import mean 
 
-pro_los = []
-dates = []
+csv_path= os.path.join("../pybank", "budget_data.csv")
 
-csv_path= os.path.join("/Users/demiurgo/Documents", "budget_data.csv")
+def total_months(dataset):
+    result = 0
+    for row in dataset:
+        result += 1
+    return result
 
-with open(csv_path, newline="") as csvfile:
-    reader = csv.reader(csv_path, delimiter=',')
-    next(reader)
+def total(dataset):
+    result = 0
+    for row in dataset:
+        result += int(row[1])
+    return result
+
+def average_change(dataset):
+    return mean(dataset)
+
+def greatest_increase(dataset):
+    result = ""
+    maximum = 0
+    index = None 
+    for row in dataset:
+        maximum = max(int(row[1]))
+        index = dataset.index(maximum)
+    result = str(dataset.pop(index))
+    return result
+
+def greatest_decrease(dataset):
+    result = ""
+    minimum = 0
+    index = None 
+    for row in dataset:
+        minimum = min(int(row[1]))
+        index = dataset.index(minimum)
+    result = str(dataset.pop(index))
+    return result
+
+with open(csv_path, "r") as csvfile:
+    reader = csv.reader(csv_path, delimiter = ",")
+    next(reader, None)
     rows = [row for row in reader]
-    for row in rows:
-        pro_los = int(row[1])
-        total_months = total_months + 1
-        dates = row[0]
-    def Average(pro_los): 
-        return mean(pro_los)
-    total = sum(pro_los)
-    average = Average(pro_los)
-    maximum = max(pro_los)
-    minimum = min(pro_los)
-    index_max = rows.index(maximum)
-    index_min = rows.index(minimum)
-    max_month = dates.pop(index_max)
-    min_month = dates.pop(index_min)
-
-output = (
-    f"Total Months: {total_months}\n"
-    f"Total : {total}\n"
-    f"Average Change: ${average}\n"
-    f"Greatest increase in Profits: {max_month} ${maximum}\n"
-    f"Greatest decrease in Profits: {min_month} ${minimum}\n"
-)
-
-print(output)
+    output_total_months = total_months(rows)
+    output_total = total(rows)
+    output_average_change = average_change(rows)
+    output_greatest_increase = greatest_increase(rows)
+    output_greatest_decrease = greatest_decrease(rows)
+    
+    output = (
+    f"Total Months: {output_total_months}\n"
+    f"Total : {output_total}\n"
+    f"Average Change: ${output_average_change}\n"
+    f"Greatest increase in Profits: {output_greatest_increase}\n"
+    f"Greatest decrease in Profits: {output_greatest_decrease}\n")
+    
+    print(output)
+    
